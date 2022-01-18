450f8fbecaf8eafbce891fd52e7a323225685954
CAG <cagonzalez1@outlook.es>
2022-01-18 00:11:27 -0500

    font erro

--- pages/index.js
+++ pages/index.js
@@ -48,7 +48,7 @@ export default function Home({posts}) {
         <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet"></link>
         
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin=''/>
 <link href="https://fonts.googleapis.com/css2?family=Orbitron&display=swap" rel="stylesheet"></link>
       </Head>
       

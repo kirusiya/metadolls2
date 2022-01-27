cc415442b934ffd1556d64f4e0fa0efbca6b6be3
CAG <cagonzalez1@outlook.es>
2022-01-27 16:07:27 -0500

    fix order text

--- components/FAQ.js
+++ components/FAQ.js
@@ -14,9 +14,13 @@ const FAQ = () => {
             
             <div className={styles.RoadMapSteps} data-aos="zoom-in"  onClick={()=>{toggleArray(0)}}>
                 <p className={styles.numerStyle}>How was the project born?</p>
-                <p  className={toggleFAQ[0] ? styles.descriptionStyleactive : styles.descriptionStyle}>This project is created by colombian artist Alejandro Rincón, tattoo artist for over 7 years. It explores the link and effect that tattoos have in the construction of our identity now a days.
+                <p  className={toggleFAQ[0] ? styles.descriptionStyleactive : styles.descriptionStyle}>This project is created by colombian artist Alejandro Rincón, tattoo artist for over 7 years. It explores the link and effect that tattoos have in the construction of our identity nowadays.
+The body is represented by the Dolls, iconic objects fabricated in mass and registered in series, which results comparable to the characteristics of human condition in today's world. Additionally the tattoo is done over the inorganic base as an artistic expression and a form of customization, which offers a way of identification or differentiation.
+<br/><br/>MetaDolls brings together the concepts of body, brand and tattoo, through a research-creation process, which explores the relationship between them and their influence on the significant construction of bodies in contemporary society. At the same time, it models the tattoo as an artistic practice that seeks to transform the individual.
+<br/><br/>Currently, cultural models and patterns have been transforming and adopting new ways of dressing or looking; artistic body modifications such as tattoos and piercings are part of these changes and therefore have aroused social interest; over time they have ceased to be stigmatized and their use has become standarized, framing a series of cultural patterns that have also undergone modifications and variations today, taking new forms in modern culture, countercultures, pop culture and now cryptoculture.
+<br/><br/>In this way, the MetaDolls are that proposal that violates the traditional models and patterns of the body and beauty. At the same time, they are an exploration of new identities and artistic expressions that are born from the need to represent the human being. When the body changes, it communicates differently, and this type of communication also transforms the very language of culture.
 
-The body is represented by the Dolls, iconic objects fabricated in mass and registered in series, which results comparable to the characteristics of human condition in today's world. Additionally the tattoo is done over the inorganic base as an artistic expression and a form of customization, which offers a way of identification or differentiation.</p>
+</p>
               
             </div>
 
--- pages/index.js
+++ pages/index.js
@@ -68,7 +68,7 @@ export default function Home({posts}) {
       <Team/>
       <FAQ/>
       
-      <GsapVideo/>
+     {/*  <GsapVideo/> */}
       {/* 
    
       

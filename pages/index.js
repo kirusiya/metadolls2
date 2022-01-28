2a1c5b072f9df8672620cc677abec39a363a1812
CAG <cagonzalez1@outlook.es>
2022-01-27 20:20:48 -0500

    changes

--- pages/index.js
+++ pages/index.js
@@ -70,7 +70,7 @@ export default function Home({posts}) {
       </div>
       <Team/>
       <FAQ/>
-      <JoinCommunity/>
+      {/* <JoinCommunity/> */}
       <Footer/>
       </div>
      {/*  <GsapVideo/> */}

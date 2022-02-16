53e9752adc8441572b4d8b80df148a2bc30f118c
CAG <cagonzalez1@outlook.es>
2022-02-15 19:09:52 -0500

    add spinner

--- components/VideoScrolling.js
+++ components/VideoScrolling.js
@@ -24,3 +24,221 @@ import styles from '../styles/VideoScrolling.module.css'
     return <div onScroll={()=>{handleScroll()}} className={styles.box} ref={boxRef}>Hello</div>;
   }
   export default VideoScrolling;
+
+
+
+  /* 
+  
+  <div
+              style={{
+                WebkitTransform: "rotate(90deg)",
+                position: "relative",
+                zIndex: 10000000000000000000000000,
+                left: "50%",
+                display: "flex",
+                justifyContent: "space-between",
+                bottom: "-670px",
+                
+              }}
+            >
+              <div >
+                <img src={"/tfile/Tattoos/boredape copia.png"} style={{ width: "5%",margin:'10px' }} />
+                <img
+                  src={"/tfile/Tattoos/brass copia.png"}
+                  style={{ width: "5%",margin:'10px' }}
+                />
+                <img
+                  src={"/tfile/Tattoos/badkid copia.png"}
+                  style={{ width: "5%",margin:'10px' }}
+                />
+                <img
+                  src={"/tfile/Tattoos/broken copia.png"}
+                  style={{ width: "5%",margin:'10px' }}
+                />
+                <img
+                  src={"/tfile/Tattoos/btc copia.png"}
+                  style={{ width: "5%" ,margin:'10px'}}
+                />
+                <img
+                  src={"/tfile/Tattoos/cat_gif1 copia.png"}
+                  style={{ width: "5%",margin:'10px' }}
+                />
+                <img
+                  src={"/tfile/Tattoos/chaos copia.png"}
+                  style={{ width: "5%",margin:'10px' }}
+                />
+                <img
+                  src={"/tfile/Tattoos/chill copia.png"}
+                  style={{ width: "5%" ,margin:'10px'}}
+                />
+                <img
+                  src={"/tfile/Tattoos/chillpill copia.png"}
+                  style={{ width: "5%",margin:'10px' }}
+                />
+                <img
+                  src={"/tfile/Tattoos/cigbox copia.png"}
+                  style={{ width: "5%" ,margin:'10px'}}
+                />
+              </div>
+            </div>
+            <div
+              style={{
+                WebkitTransform: "rotate(90deg)",
+                position: "relative",
+                zIndex: 10000000000000000000000000,
+                right: "50%",
+                display: "flex",
+                justifyContent: "space-between",
+                bottom: "-570px",
+                
+              }}
+            >
+              <div>
+              <img src={"/tfile/Tattoos/boredape copia.png"} style={{ width: "5%",margin:'10px' }} />
+                <img
+                  src={"/tfile/Tattoos/brass copia.png"}
+                  style={{ width: "5%",margin:'10px' }}
+                />
+                <img
+                  src={"/tfile/Tattoos/badkid copia.png"}
+                  style={{ width: "5%",margin:'10px' }}
+                />
+                <img
+                  src={"/tfile/Tattoos/broken copia.png"}
+                  style={{ width: "5%",margin:'10px' }}
+                />
+                <img
+                  src={"/tfile/Tattoos/btc copia.png"}
+                  style={{ width: "5%" ,margin:'10px'}}
+                />
+                <img
+                  src={"/tfile/Tattoos/cat_gif1 copia.png"}
+                  style={{ width: "5%",margin:'10px' }}
+                />
+                <img
+                  src={"/tfile/Tattoos/chaos copia.png"}
+                  style={{ width: "5%",margin:'10px' }}
+                />
+                <img
+                  src={"/tfile/Tattoos/chill copia.png"}
+                  style={{ width: "5%" ,margin:'10px'}}
+                />
+                <img
+                  src={"/tfile/Tattoos/chillpill copia.png"}
+                  style={{ width: "5%",margin:'10px' }}
+                />
+                <img
+                  src={"/tfile/Tattoos/cigbox copia.png"}
+                  style={{ width: "5%" ,margin:'10px'}}
+                />
+              </div>
+            </div>
+  
+
+
+            <div
+              style={{
+                WebkitTransform: "rotate(90deg)",
+                position: "relative",
+                zIndex: 10000000000000000000000000,
+                left: "50%",
+                display: "flex",
+                justifyContent: "space-between",
+                bottom: "-670px",
+                
+              }}
+            >
+              <div >
+                <img src={"/tfile/Tattoos/baby1 copia.png"} style={{ width: "5%",margin:'10px' }} />
+                <img
+                  src={"/tfile/Tattoos/badday copia.png"}
+                  style={{ width: "5%",margin:'10px' }}
+                />
+                <img
+                  src={"/tfile/Tattoos/badkid copia.png"}
+                  style={{ width: "5%",margin:'10px' }}
+                />
+                <img
+                  src={"/tfile/Tattoos/balance copia.png"}
+                  style={{ width: "5%",margin:'10px' }}
+                />
+                <img
+                  src={"/tfile/Tattoos/bandaid copia.png"}
+                  style={{ width: "5%" ,margin:'10px'}}
+                />
+                <img
+                  src={"/tfile/Tattoos/BarCode copia.png"}
+                  style={{ width: "5%",margin:'10px' }}
+                />
+                <img
+                  src={"/tfile/Tattoos/basquiat1 copia.png"}
+                  style={{ width: "5%",margin:'10px' }}
+                />
+                <img
+                  src={"/tfile/Tattoos/bayc copia.png"}
+                  style={{ width: "5%" ,margin:'10px'}}
+                />
+                <img
+                  src={"/tfile/Tattoos/binance.png"}
+                  style={{ width: "5%",margin:'10px' }}
+                />
+                <img
+                  src={"/tfile/Tattoos/bones copia.png"}
+                  style={{ width: "5%" ,margin:'10px'}}
+                />
+              </div>
+            </div>
+            <div
+              style={{
+                WebkitTransform: "rotate(90deg)",
+                position: "relative",
+                zIndex: 10000000000000000000000000,
+                right: "50%",
+                display: "flex",
+                justifyContent: "space-between",
+                bottom: "-570px",
+                
+              }}
+            >
+              <div>
+              <img src={"/tfile/Tattoos/baby1 copia.png"} style={{ width: "5%",margin:'10px' }} />
+                <img
+                  src={"/tfile/Tattoos/badday copia.png"}
+                  style={{ width: "5%",margin:'10px' }}
+                />
+                <img
+                  src={"/tfile/Tattoos/badkid copia.png"}
+                  style={{ width: "5%",margin:'10px' }}
+                />
+                <img
+                  src={"/tfile/Tattoos/balance copia.png"}
+                  style={{ width: "5%",margin:'10px' }}
+                />
+                <img
+                  src={"/tfile/Tattoos/bandaid copia.png"}
+                  style={{ width: "5%" ,margin:'10px'}}
+                />
+                <img
+                  src={"/tfile/Tattoos/BarCode copia.png"}
+                  style={{ width: "5%",margin:'10px' }}
+                />
+                <img
+                  src={"/tfile/Tattoos/basquiat1 copia.png"}
+                  style={{ width: "5%",margin:'10px' }}
+                />
+                <img
+                  src={"/tfile/Tattoos/bayc copia.png"}
+                  style={{ width: "5%" ,margin:'10px'}}
+                />
+                <img
+                  src={"/tfile/Tattoos/binance.png"}
+                  style={{ width: "5%",margin:'10px' }}
+                />
+                <img
+                  src={"/tfile/Tattoos/bones copia.png"}
+                  style={{ width: "5%" ,margin:'10px'}}
+                />
+              </div>
+            </div>
+  
+  */

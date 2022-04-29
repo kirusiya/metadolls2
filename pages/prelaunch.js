97f508636bebd2b81e5ae838e5989ebae0b8b661
kirusiya <edward.avalos.severiche@gmail.com>
2022-04-28 21:15:32 -0400

    Update prelaunch.js

--- pages/prelaunch.js
+++ pages/prelaunch.js
@@ -117,7 +117,10 @@ export default function Prelauch({ posts }) {
         />
         <link href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@500&family=Redressed&family=Ubuntu:wght@300&display=swap" rel="stylesheet"></link>
        
-        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css"/>
+        <link 
+          href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css"
+          rel="stylesheet"        
+        />
 
        
        {/*  <link
